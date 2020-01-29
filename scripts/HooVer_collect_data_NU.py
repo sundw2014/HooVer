704a3a66a0ab4fee30e04dbea7ef9ad25a824d2f
sundw2014 <sundw2014@gmail.com>
2020-01-29 01:16:49 -0600

    table3

---
+++ scripts/HooVer_collect_data_NU.py
@@ -0,0 +1,39 @@
+# Author: Rajat Sen # Modified by Negin
+import sys
+sys.path.append('..')
+import numpy as np
+import subprocess
+from subprocess import DEVNULL, STDOUT, check_call
+import os, signal
+
+from utils.general_utils import loadpklz, savepklz
+
+# -----------------------------------------------------------------------------
+
+useHOO = True
+model = 'FakeModel'
+T = 1
+dim = 2
+exp_id = int(sys.argv[1])
+port_base = 9100
+plasmalab_root = '/home/daweis2/plasmalab-1.4.4/'
+
+import models.FakeModel as simulator
+
+if __name__ == '__main__':
+    budget = 400000
+    results = []
+    original_results = []
+    ss = np.array([1.7, 2.6, 2.9, 3.2, 3.6]) * 1e-4
+    print(ss)
+    for s in ss:
+        filename = 'data/HooVer_%s_budget%d_s%lf_exp%d.pklz'%(model, budget, s, exp_id)
+        os.system('cd ../; python example.py --nRuns 1 --sigma 1e-5 --model %s_%lf --budget %d --filename %s'%(model, s, budget, filename))
+        simulator.s = s
+        initial_states = loadpklz('../'+filename)['optimal_xs'][0]
+        original_results.append(loadpklz('../'+filename)['optimal_values'][0])
+        result = simulator.get_prob(initial_states)
+        results.append(result)
+
+    savepklz({'results':results, 'ss':ss, 'original_results':original_results}, '../data/HooVer_%s_exp%d.pklz'%(model, exp_id))
+    # from IPython import embed; embed()
---
+++ scripts/PlasmaLab_collect_data_NU.py
@@ -0,0 +1,67 @@
+import sys
+sys.path.append('..')
+import numpy as np
+import subprocess
+from subprocess import DEVNULL, STDOUT, check_call
+import os, signal
+
+from utils.general_utils import loadpklz, savepklz
+
+model = 'FakeModel'
+T = 1
+dim = 2
+exp_id = int(sys.argv[1])
+port_base = 9100
+plasmalab_root = '/home/daweis2/plasmalab-1.4.4/'
+
+import models.FakeModel as FakeModel
+
+if __name__ == '__main__':
+    budget = int(400000 / 16)
+    ss = np.array([1.7, 2.6, 2.9, 3.2, 3.6]) * 1e-4
+    #epsilon = 0.01
+    delta = 0.01
+
+    port = port_base + exp_id
+    tmp_model_name = 'model_%d'%port
+    tmp_spec_name = 'spec_%d'%port
+    with open(tmp_model_name, 'w') as f:
+        f.write('%d %d %d'%(dim, T+1, port))
+    with open(tmp_spec_name, 'w') as f:
+        f.write('F<=1000 (T<=%d & US>0)'%T)
+
+    results = []
+    original_results = []
+
+    for s in ss:
+        FakeModel.s = s
+        #delta = 2 / np.exp((budget*0.8)*2*(epsilon**2))
+        epsilon = np.sqrt(np.log(2/delta)/np.log(np.e)/2/(budget*0.8))
+        print(epsilon, delta, budget, s)
+        # The os.setsid() is passed in the argument preexec_fn so
+        # it's run after the fork() and before  exec() to run the shell.
+        simulator = subprocess.Popen('cd ../; python simulator.py --model %s_%lf --port %d'%(model, s, port), shell=True, preexec_fn=os.setsid, stdout=DEVNULL)
+        output = subprocess.check_output(plasmalab_root+'/plasmacli.sh launch -m '+tmp_model_name+':PythonSimulatorBridge -r '+tmp_spec_name+':bltl -a smartsampling -A"Maximum"=True -A"Epsilon"=%lf -A"Delta"=%lf -A"Budget"=%d'%(epsilon, delta, budget), universal_newlines=True, shell=True)
+        os.killpg(os.getpgid(simulator.pid), signal.SIGTERM)  # Send the signal to all the process groups
+        with open('../data/PlasmaLab_%s_epsilon%lf_delta%lf_budget%d_s%lf_exp%d.txt'%(model, epsilon, delta, budget, s, exp_id), 'w') as f:
+            f.write(output)
+
+        with open('../data/PlasmaLab_%s_epsilon%lf_delta%lf_budget%d_s%lf_exp%d.txt'%(model, epsilon, delta, budget, s, exp_id), 'r') as f:
+            output = f.readlines()
+
+        # Strips the newline character
+        output = [line.strip() for line in output]
+        seeds = output[1:-6]
+        final_iter = [int(line.split(' ')[3]) for line in seeds[-budget+10::]]
+        final_iter = set(final_iter)
+        original_results.append(float(output[-2].split('|')[2]))
+
+        tmp_results = []
+        print(final_iter)
+        for seed in final_iter:
+            state = FakeModel.random_initialization(seed)[:-2]
+            tmp_results.append(FakeModel.get_prob(state))
+        results.append(np.max(tmp_results))
+
+savepklz({'results':results, 'ss':ss, 'original_results':original_results}, '../data/PlasmaLab_%s_exp%d.pklz'%(model, exp_id))
+os.system('rm '+tmp_model_name+' '+tmp_spec_name)
