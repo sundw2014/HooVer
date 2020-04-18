9ebc8e79315ddb8ee2393b4aacb53d61e328eaff
sundw2014 <sundw2014@gmail.com>
2020-04-18 13:12:40 -0500

    update

--- scripts/reproduce.sh
+++ scripts/reproduce.sh
@@ -18,7 +18,7 @@ parallel -u python -u PlasmaLab_collect_data_NU_nqueries_regret.py ::: 1 2 3 4 5
 parallel -u python -u HooVer_collect_data_SL.py ::: 1 2 3 4 5 6 7 8 9 10
 parallel -u python -u PlasmaLab_collect_data_SL.py ::: 1 2 3 4 5 6 7 8 9 10
 
-#parallel -u python -u HooVer_collect_data_SL_bs.py ::: 1 2 3 4 5 6 7 8 9 10
+parallel -u python -u HooVer_collect_data_SL_bs.py ::: 1 2 3 4 5 6 7 8 9 10
 parallel -u python -u HooVer_collect_data_SL_rhomax.py ::: 1 2 3 4 5 6 7 8 9 10
 parallel -u python -u HooVer_collect_data_SL_nqueries_regret_bs.py ::: 1 2 3 4 5 6 7 8 9 10
 
